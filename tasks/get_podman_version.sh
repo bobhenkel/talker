9ce92d76abdd52e97b17f066563816f3eff56c6e
Bob Henkel <bhenkel@cray.com>
2020-12-17 17:15:35 -0600

    bug: Fix spelling of command.

--- tasks/get_podman_version.sh
+++ tasks/get_podman_version.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #Help from https://stackoverflow.com/questions/592620/how-can-i-check-if-a-program-exists-from-a-bash-script
 if hash podman 2>/dev/null; then
-    pomdan info
+    podman info
 else
     echo "COMMAND could not be found"
     exit
