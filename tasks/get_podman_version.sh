88fffb82c707a34bdaedc2604a54894f458008ce
Bob Henkel <bhenkel@cray.com>
2020-12-17 17:13:03 -0600

    feature: Try idempotency shell script.

---
+++ tasks/get_podman_version.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+#Help from https://stackoverflow.com/questions/592620/how-can-i-check-if-a-program-exists-from-a-bash-script
+if hash podman 2>/dev/null; then
+    pomdan info
+else
+    echo "COMMAND could not be found"
+    exit
+fi
